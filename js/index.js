d8ef6c040091d04476aaabe1fb4cc1477570c9f2
Carlo Gigante <124770287+CarloGigante@users.noreply.github.com>
2023-11-10 22:35:18 +0800

    hero update

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const textArray = ["Web Developer", "Designer", "Creative"];
+const typingTexts = document.querySelectorAll(".typing-text");
+
+typingTexts.forEach((typingText, index) => {
+    let textIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < textArray[textIndex].length) {
+            typingText.textContent += textArray[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 1000);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, 100);
+        } else {
+            textIndex = (textIndex + 1) % textArray.length;
+            setTimeout(type, 500);
+        }
+    }
+
+    setTimeout(type, index * 2000); // Delay the animation of each span
+});
